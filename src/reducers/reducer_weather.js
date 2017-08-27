15f38fd73cd9c8d993b66297034a2fe30765c2c7
danale <danale@danales-MacBook-Pro.local>
2017-08-26 22:08:56 -0400

    Built List Container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,12 @@
-export default function(state = null, action) {
-  // reducer takes an action and console logs it
-  console.log('Action received', action);
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // inserting new record at top of array
+      // this does not manipulate state
+      return [action.payload.data, ...state]; // [city, city, city]
+  }
   // doesnt do anything with state
   return state;
 }
